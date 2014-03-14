1ce85e3833bb00769f1d6af71e0ea8c3ac8faba6
visumdesignz <mike@visumdesignz.com>
2014-03-14 04:41:38 -0500

    Update contact_details.js
    Added a way to destroy() the model inside the gulp.js branch version.

--- client/src/views/contact_details.js
+++ client/src/views/contact_details.js
@@ -3,11 +3,25 @@ var Marionette = require('backbone.marionette');
 module.exports = ContactDetailsView = Marionette.ItemView.extend({
     template: require('../../templates/contact_details.hbs'),
     events: {
-        'click a': 'goBack'
+        'click a.back': 'goBack',
+        'click a.delete': 'deleteContact'
     },
 
     goBack: function(e) {
         e.preventDefault();
+        window.App.controller.home();
+    },
+    deleteContact: function(e) {
+        e.preventDefault();
+        console.log('Deleting contact');
+
+        // this will actually send a DELETE to the server:
+        this.model.destroy({
+          success: function(model, response)  {
+              window.App.data.contacts.remove(this.model);
+          }
+        });
+
         window.App.controller.home();
     }
 });
